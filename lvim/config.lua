e15905a512aff6f683afc507ad334e88f4244b12
nanchano <nicolas.anchanio@gmail.com>
2022-12-12 17:23:21 -0300

    black formatting on isort

--- lvim/config.lua
+++ lvim/config.lua
@@ -148,7 +148,7 @@ require("lvim.lsp.manager").setup("pyright", opts)
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
     { command = "black", filetypes = { "python" } },
-    { command = "isort", filetypes = { "python" } },
+    { command = "isort", filetypes = { "python" }, extra_args = { "--profile", "black" } },
     { command = "gofmt", filetypes = { "go" } },
     --   {
     --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
